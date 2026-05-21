77242a578d6c235fe2aac18fcd16743649cd9300
IsmaelNjama <petermailsnjama@gmail.com>
2026-05-21 20:55:01 +0300

    tools complete

--- main.py
+++ main.py
@@ -1,12 +1,14 @@
-from tools.tools import tools
-from utils.prompt import prompt
 import os
 from dotenv import load_dotenv
-
 from langchain_openai import ChatOpenAI
+from langchain_classic.agents import (
+    AgentExecutor,
+    create_tool_calling_agent,
+)
 
+from tools.tools import tools
+from utils.system_prompt import system_prompt
 
-from langchain.agents import create_agent
 
 load_dotenv()
 
@@ -15,18 +17,15 @@ llm = ChatOpenAI(
     base_url="https://api.tokenfactory.us-central1.nebius.com/v1/",
     api_key=os.environ.get("NEBIUS_API_KEY")
 )
-prompt = prompt
-agent = create_agent(
-    model=llm,
-    tools=[tools],
-    system_prompt="""
-You are a customer service data analyst assistant.
-You have access to a customer service dataset and can query it using the tools provided.
-Answer the user's question as accurately as possible using the tools.
-"""
+
+
+agent = create_tool_calling_agent(llm, tools, system_prompt)
+
+executor = AgentExecutor(
+    agent=agent,
+    tools=tools,
+    max_iterations=10,
+    verbose=True
 )
-print(agent.invoke({"input": "What categories exist in the dataset"}))
-# agent = create_tool_calling_agent(llm, tools=tools, prompt=prompt)
-# executor = AgentExecutor(agent=agent, tools=tools,
-#                          max_iterations=10, verbose=True)
-# executor.invoke({"input": "What categories exist in the dataset"})
+
+executor.invoke({"input": "Summarize the FEEDBACK category."})
